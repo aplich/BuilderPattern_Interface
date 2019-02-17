5272f1005f70e605634946297c3bd582c60593ad
Arek Plich <36642023+aplich@users.noreply.github.com>
2019-02-17 21:09:14 +0100

    Update House.java

--- src/House.java
+++ src/House.java
@@ -1,11 +1,11 @@
 public class House {
-    public String walls;
-    public String floors;
-    public String rooms;
-    public String roof;
-    public String windows;
-    public String doors;
-    public String garage;
+    private String walls;
+    private String floors;
+    private String rooms;
+    private String roof;
+    private String windows;
+    private String doors;
+    private String garage;
 
     public String getWalls() {
         return walls;
